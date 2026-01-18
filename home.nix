7f34b4837e4f16157793eab0d20b3a1a21572157
Woojong Koh <wjngkoh@gmail.com>
2026-01-18 22:53:24 +0900

    Update home.packages

--- home.nix
+++ home.nix
@@ -4,8 +4,6 @@
   neovim,
   ...
 }: {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "wjkoh";
   home.homeDirectory = "/Users/wjkoh";
 
@@ -18,68 +16,88 @@
   # release notes.
   home.stateVersion = "25.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
     neovim.packages.${pkgs.system}.default
-
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    pkgs.bat
+    pkgs.git
+    pkgs.lazygit
+    pkgs.fx
+    pkgs.gh
+    pkgs.curl
+    pkgs.tldr
+    pkgs.lazydocker
+    pkgs.btop
+    pkgs.nerd-fonts.jetbrains-mono
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/wjkoh/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "nvim";
+    VISUAL = "nvim";
+    PAGER = "bat";
+    MANPAGER = "nvim +Man!";
+    LANG = "en_US.UTF-8";
   };
+
+  xdg.configFile."ghostty/config".text = ''
+    theme = Catppuccin Mocha
+    font-family = JetBrainsMono Nerd Font Mono
+    font-size = 14
+    window-padding-x = 10
+    window-padding-y = 10
+    macos-titlebar-style = transparent
+  '';
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ls = "eza --icons";
+      ll = "eza -l --icons --git -a";
+      lt = "eza --tree --level=2 --long --icons --git";
+      cat = "bat";
+      g = "git";
+      lg = "lazygit";
+      ld = "lazydocker";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.eza = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.delta = {
+    enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Woojong Koh";
+        email = "wjngkoh@gmail.com";
+      };
+      init.defaultBranch = "main";
+      pull.rebase = true;
+    };
+  };
 
   programs.direnv = {
     enable = true;
