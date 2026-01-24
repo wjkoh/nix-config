d34755b018ca4a65e950a3b045c70bb6e059780c
Woojong Koh <wjngkoh@gmail.com>
2026-01-24 16:50:27 +0900

    refactor: simplify supported systems list

--- flake.nix
+++ flake.nix
@@ -23,11 +23,8 @@
   } @ inputs: let
     inherit (self) outputs;
     systems = [
-      "aarch64-linux"
-      "i686-linux"
       "x86_64-linux"
       "aarch64-darwin"
-      "x86_64-darwin"
     ];
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
