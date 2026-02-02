4d1d7e9f3511ba7fe9916b7832bcaa7efb062e9b
Woojong Koh <wjngkoh@gmail.com>
2026-02-03 03:32:34 +0900

    fix: resolve deprecated system warning and suppress options.json warning

--- flake.nix
+++ flake.nix
@@ -54,11 +54,15 @@
     # Available through 'nixos-rebuild --flake .#z2-mini'
     nixosConfigurations = {
       "z2-mini" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
         specialArgs = {inherit inputs outputs;};
         modules = [
+          {nixpkgs.hostPlatform = "x86_64-linux";}
+
           # Main NixOS configuration file
           ./hosts/z2-mini/configuration.nix
+
+          # Enable unfree packages
+          {nixpkgs.config.allowUnfree = true;}
 
           # Home Manager module
           home-manager.nixosModules.home-manager
@@ -69,7 +73,10 @@
               inherit inputs outputs neovim;
               llamaCppPackage = nixpkgs.legacyPackages.x86_64-linux.llama-cpp.override {vulkanSupport = true;};
             };
-            home-manager.users.wjkoh = import ./hosts/z2-mini/home.nix;
+            home-manager.users.wjkoh = {
+              imports = [./hosts/z2-mini/home.nix];
+              manual.html.enable = false;
+            };
           }
         ];
       };
@@ -79,9 +86,10 @@
     # Available through 'darwin-rebuild switch --flake .#mbp-14'
     darwinConfigurations = {
       "mbp-14" = nix-darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
         specialArgs = {inherit inputs outputs;};
         modules = [
+          {nixpkgs.hostPlatform = "aarch64-darwin";}
+
           ./hosts/mbp-14/configuration.nix
           home-manager.darwinModules.home-manager
           {
@@ -91,7 +99,10 @@
               inherit inputs outputs neovim;
               llamaCppPackage = nixpkgs.legacyPackages.aarch64-darwin.llama-cpp;
             };
-            home-manager.users.wjkoh = import ./hosts/mbp-14/home.nix;
+            home-manager.users.wjkoh = {
+              imports = [./hosts/mbp-14/home.nix];
+              manual.html.enable = false;
+            };
           }
         ];
       };
