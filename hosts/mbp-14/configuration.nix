2d2d17c289ba31c8310af954dc3d885703b57885
Woojong Koh <wjngkoh@gmail.com>
2026-02-02 23:54:49 +0900

    feat: migrate mbp-14 to nix-darwin

---
+++ hosts/mbp-14/configuration.nix
@@ -0,0 +1,41 @@
+{pkgs, ...}: {
+  # Nix configuration ------------------------------------------------------------------------------
+  nix.enable = false;
+  # nix.settings.experimental-features = ["nix-command" "flakes"];
+  nixpkgs.config.allowUnfree = true;
+
+  # Keyboard
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToEscape = true;
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  # System preferences
+  system.defaults = {
+    dock = {
+      autohide = true;
+      show-recents = false;
+    };
+    finder = {
+      AppleShowAllExtensions = true;
+      ShowPathbar = true;
+    };
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      KeyRepeat = 2;
+    };
+  };
+
+  # Backward compatibility
+  system.stateVersion = 5;
+
+  # User setup
+  users.users.wjkoh = {
+    name = "wjkoh";
+    home = "/Users/wjkoh";
+  };
+
+  # Set the primary user for user-dependent defaults
+  system.primaryUser = "wjkoh";
+}
