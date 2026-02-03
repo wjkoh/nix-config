6f77f0dfb221d132df85422797a2ede074db05fb
Woojong Koh <wjngkoh@gmail.com>
2026-02-03 14:44:44 +0900

    feat(gui): configure karabiner to switch input to English on Escape

--- hosts/mbp-14/configuration.nix
+++ hosts/mbp-14/configuration.nix
@@ -8,6 +8,8 @@
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToControl = true;
 
+  services.karabiner-elements.enable = true;
+
   # Add ability to used TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
 
--- modules/home/gui.nix
+++ modules/home/gui.nix
@@ -7,4 +7,45 @@
     window-padding-y = 10
     macos-titlebar-style = transparent
   '';
+
+  xdg.configFile."karabiner/karabiner.json".text = builtins.toJSON {
+    global = {
+      check_for_updates_on_startup = true;
+      show_in_menu_bar = true;
+      show_profile_name_in_menu_bar = false;
+    };
+    profiles = [
+      {
+        name = "Default";
+        complex_modifications = {
+          rules = [
+            {
+              description = "Escape to English";
+              manipulators = [
+                {
+                  type = "basic";
+                  from = {
+                    key_code = "escape";
+                    modifiers = {
+                      optional = ["any"];
+                    };
+                  };
+                  to = [
+                    {key_code = "escape";}
+                    {select_input_source = {language = "^en$";};}
+                  ];
+                }
+              ];
+            }
+          ];
+        };
+        devices = [];
+        fn_function_keys = [];
+        simple_modifications = [];
+        virtual_hid_keyboard = {
+          keyboard_type_v2 = "ansi";
+        };
+      }
+    ];
+  };
 }
