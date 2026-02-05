bc1ff23a1109fd0d376bad6e098f018ff685ab0b
Woojong Koh <wjngkoh@gmail.com>
2026-02-05 17:43:42 +0900

    refactor(zsh): rename initExtra to initContent to resolve deprecation

--- hosts/z2-mini/home.nix
+++ hosts/z2-mini/home.nix
@@ -4,7 +4,7 @@
   home.username = "wjkoh";
   home.homeDirectory = "/home/wjkoh";
 
-  programs.zsh.initExtra = ''
+  programs.zsh.initContent = ''
     # Auto-start Zellij on SSH
     if [[ -n "$SSH_CONNECTION" ]] && [[ -z "$ZELLIJ" ]]; then
         exec zellij attach --create default
