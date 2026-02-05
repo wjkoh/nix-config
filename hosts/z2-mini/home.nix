6c5a74c452936f3f8ae407f8831246df0a6f6102
Woojong Koh <wjngkoh@gmail.com>
2026-02-05 17:42:10 +0900

    feat(z2-mini): auto-start zellij on ssh

--- hosts/z2-mini/home.nix
+++ hosts/z2-mini/home.nix
@@ -3,4 +3,11 @@
 
   home.username = "wjkoh";
   home.homeDirectory = "/home/wjkoh";
+
+  programs.zsh.initExtra = ''
+    # Auto-start Zellij on SSH
+    if [[ -n "$SSH_CONNECTION" ]] && [[ -z "$ZELLIJ" ]]; then
+        exec zellij attach --create default
+    fi
+  '';
 }
