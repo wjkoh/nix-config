620f142f6f47bd99382a0555cd1d11b55d890998
Woojong Koh <wjngkoh@gmail.com>
2026-02-05 16:16:40 +0900

    fix(karabiner): force overwrite of karabiner.json

--- modules/home/karabiner.nix
+++ modules/home/karabiner.nix
@@ -1,40 +1,43 @@
 {pkgs, ...}: {
-  xdg.configFile."karabiner/karabiner.json".text = builtins.toJSON {
-    profiles = [
-      {
-        name = "Default";
-        complex_modifications = {
-          rules = [
-            {
-              description = "Caps Lock to Ctrl (Hold) / Esc (Tap)";
-              manipulators = [
-                {
-                  from = {
-                    key_code = "caps_lock";
-                    modifiers = {optional = ["any"];};
-                  };
-                  to = [{key_code = "left_control";}];
-                  to_if_alone = [{key_code = "escape";}];
-                  type = "basic";
-                }
-              ];
-            }
-            {
-              description = "Esc switches to English";
-              manipulators = [
-                {
-                  from = {key_code = "escape";};
-                  to = [
-                    {key_code = "escape";}
-                    {select_input_source = {language = "^en$";};}
-                  ];
-                  type = "basic";
-                }
-              ];
-            }
-          ];
-        };
-      }
-    ];
+  xdg.configFile."karabiner/karabiner.json" = {
+    force = true;
+    text = builtins.toJSON {
+      profiles = [
+        {
+          name = "Default";
+          complex_modifications = {
+            rules = [
+              {
+                description = "Caps Lock to Ctrl (Hold) / Esc (Tap)";
+                manipulators = [
+                  {
+                    from = {
+                      key_code = "caps_lock";
+                      modifiers = {optional = ["any"];};
+                    };
+                    to = [{key_code = "left_control";}];
+                    to_if_alone = [{key_code = "escape";}];
+                    type = "basic";
+                  }
+                ];
+              }
+              {
+                description = "Esc switches to English";
+                manipulators = [
+                  {
+                    from = {key_code = "escape";};
+                    to = [
+                      {key_code = "escape";}
+                      {select_input_source = {language = "^en$";};}
+                    ];
+                    type = "basic";
+                  }
+                ];
+              }
+            ];
+          };
+        }
+      ];
+    };
   };
 }
